c123efdaf6e4e3309b0601d99071a993c36dcb76
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-12-10 14:10:18 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -2,9 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test",(req, res) => {
+const {adminAuth} = require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+
+/* app.get("/user", (req, res) => {
+    res.send("");
+})
+ */
+
+app.get("/admin/getAllData", (req, res) => {
+    res.send("User Data Send");
+})
+app.get("/admin/deleteUser", (req, res) => {
+    res.send("User Data Deleted.");
+})
+
+/* app.use("/test",(req, res) => {
     res.send("Hello from the server!!..");
-});
+}); */
 
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000....");

31fd9bd9c7c92c2fc788fdbc3595da64972b604c
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-12-12 00:24:35 +0530

    add validation to the schema level

--- src/models/user.js
+++ src/models/user.js
@@ -3,24 +3,50 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
+    minLength:4,
+    maxLength:30,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+    unique: true,
   },
   password: {
     type: String,
+    require: true,
   },
   age: {
     type: Number,
+    min:18,
   },
   gender: {
     type: String,
+    validate(value) {
+      if(!["male", "female", "others"].includes(value)){
+        throw new Error("Gender data is not valid");
+      }
+    },
   },
+  photoUrl: {
+    type: String,
+    default:
+      "https://www.pngitem.com/middle/ixJxmw_profile-pic-dummy-png-transparent-png/",
+  },
+  about: {
+    type: String,
+    default: "This is a default about of the user!",
+  },
+  skills: {
+    type: [String],
+  },
+}, {
+  timestamps:true,
 });
 
 module.exports = mongoose.model("User", userSchema);
-
-

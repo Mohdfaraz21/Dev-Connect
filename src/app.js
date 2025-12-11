5cba1361f7984a06fa4f7247cabca4b3695abfc4
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-12-12 00:46:53 +0530

    add validation at api level

--- src/app.js
+++ src/app.js
@@ -18,16 +18,15 @@ app.post("/signup", async (req, res) => {
 
 /* @get user by Email */
 app.get("/user", async (req, res) => {
-
   try {
-  const user = await User.findOne({emailId: userEmail});
-  if(!user) {
-    res.status(404).send("User not found");
-  } else {
-    res.send(user);
-  }
-  
-  /* const userEmail = req.body.emailId;
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+
+    /* const userEmail = req.body.emailId;
   try {
     const users = await User.find({ emailId: userEmail });
     if (users.length === 0) {
@@ -37,7 +36,7 @@ app.get("/user", async (req, res) => {
     } */
   } catch (err) {
     res.status(400).send("Something went wrong!!");
-  } 
+  }
 });
 
 /* @Feed api  - GET all the users from the database */
@@ -47,6 +46,51 @@ app.get("/feed", async (req, res) => {
     res.send(users);
   } catch (err) {
     res.status(400).send("Something went wrong!!");
+  }
+});
+
+/* @delete api */
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+
+    res.send("User deleted Successfully...");
+  } catch (err) {
+    res.status(400).send("Something went wrong!!");
+  }
+});
+
+/* @update data of the user */
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_UPDATES = [
+      "userId",
+      "photoUrl",
+      "about",
+      "gender",
+      "age",
+      "skills",
+    ];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed!!.");
+    }
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+    await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("User updated successfully...");
+  } catch (err) {
+    res.status(400).send("Failed to Update:" + err.message);
   }
 });
 

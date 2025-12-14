7b81abd57cfe59448b97aa0401f2928cd0ca1897
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-12-14 12:31:19 +0530

    added auth middleware

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const { userAuth } = require("./middlewares/auth");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -65,110 +66,22 @@ app.post("/login", async (req, res) => {
 });
 
 //@profile API
-app.get("/profile", async (req, res) => {
-
+app.get("/profile", userAuth, async (req, res) => {
   try {
-    const cookies = req.cookies;
-    
-    const { token } = cookies;
-    
-      if(!token) {
-        throw new Error("Invalid credentials!")
-      }
-
-    // Validate my token
-    const messageDecoded = await jwt.verify(token, "Dev-Connect@22");
-    const { _id } = messageDecoded;
-    console.log("Logged In user id: " + _id);
-    
-    const user = await User.findById(_id);
-    if(!user) {
-      throw new Error("User does not exist.")
-    }
-    
+    const user = req.user;
     res.send(user);
+
+
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
   }
 });
 
-/* @get user by Email */
-app.get("/user", async (req, res) => {
-  try {
-    const user = await User.findOne({ emailId: userEmail });
-    if (!user) {
-      res.status(404).send("User not found");
-    } else {
-      res.send(user);
-    }
 
-    /* const userEmail = req.body.emailId;
-  try {
-    const users = await User.find({ emailId: userEmail });
-    if (users.length === 0) {
-      res.status(404).send("User not found!!");
-    } else {
-      res.send(users);
-    } */
-  } catch (err) {
-    res.status(400).send("Something went wrong!!");
-  }
-});
 
-/* @Feed api  - GET all the users from the database */
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (err) {
-    res.status(400).send("Something went wrong!!");
-  }
-});
 
-/* @delete api */
-app.delete("/user", async (req, res) => {
-  const userId = req.body.userId;
-  try {
-    const user = await User.findByIdAndDelete(userId);
 
-    res.send("User deleted Successfully...");
-  } catch (err) {
-    res.status(400).send("Something went wrong!!");
-  }
-});
 
-/* @update data of the user */
-app.patch("/user/:userId", async (req, res) => {
-  const userId = req.params?.userId;
-  const data = req.body;
-
-  try {
-    const ALLOWED_UPDATES = [
-      "userId",
-      "photoUrl",
-      "about",
-      "gender",
-      "age",
-      "skills",
-    ];
-    const isUpdateAllowed = Object.keys(data).every((k) =>
-      ALLOWED_UPDATES.includes(k)
-    );
-    if (!isUpdateAllowed) {
-      throw new Error("Update not allowed!!.");
-    }
-    if (data?.skills.length > 10) {
-      throw new Error("Skills cannot be more than 10");
-    }
-    await User.findByIdAndUpdate({ _id: userId }, data, {
-      returnDocument: "after",
-      runValidators: true,
-    });
-    res.send("User updated successfully...");
-  } catch (err) {
-    res.status(400).send("Failed to Update:" + err.message);
-  }
-});
 
 connectDB()
   .then(() => {
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,15 +1,28 @@
-const adminAuth = (req, res, next) => {
-  console.log("Admin auth is getting checked!!!");
-  const token = "xyz";
-  const isAdminAuthorized = token === "xyz";
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-  if (!isAdminAuthorized) {
-    res.status(401).send("Unauthorized Access");
-  } else {
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Token is not Valid!.");
+    }
+
+    const decodedObj = await jwt.verify(token, "Dev-Connect@22");
+
+    const { _id } = decodedObj;
+
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    req.user = user;
     next();
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
 };
 
 module.exports = {
-  adminAuth,
+  userAuth,
 };

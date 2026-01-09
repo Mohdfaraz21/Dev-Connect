59166130d5c4bf9125625896c1b21a7a6c8246bf
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-01-09 20:16:55 +0530

    added connection request DBmodel

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["interested", "ignored", "accepted", "rejected"],
+        message: `{VALUE} is not valid type`,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
+connectionRequestSchema.pre("save", async function () {
+  if (this.fromUserId.equals(this.toUserId)) {
+    throw new Error("Cannot send connection request to yourself!");
+  }
+});
+
+const connectionRequestModel = new mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
+
+module.exports = connectionRequestModel;

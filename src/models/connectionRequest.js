00aca363b8568f0f3cd9e0800371cf1060ec1afe
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-01-30 20:33:08 +0530

    added compound indexes

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,12 +1,15 @@
+const { request } = require("express");
 const mongoose = require("mongoose");
 
 const connectionRequestSchema = new mongoose.Schema(
   {
     fromUserId: {
       type: mongoose.Schema.Types.ObjectId,
+      required:true
     },
     toUserId: {
       type: mongoose.Schema.Types.ObjectId,
+      request:true,
     },
     status: {
       type: String,
@@ -25,6 +28,7 @@ const connectionRequestSchema = new mongoose.Schema(
 
 connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
 
+//@anytime when you call the save before run
 connectionRequestSchema.pre("save", async function () {
   if (this.fromUserId.equals(this.toUserId)) {
     throw new Error("Cannot send connection request to yourself!");

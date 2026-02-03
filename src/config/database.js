fa18bac222aea165145884034ef7618c7a9be2c9
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-02-03 13:02:57 +0530

    chnages in db

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://faraz:Faraz22@dev-connect.pajlzum.mongodb.net/Dev-connect");
+    "");
 };
 
 module.exports = connectDB;

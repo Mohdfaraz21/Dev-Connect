2121019ff7397a69b8b90d47e1fdc0d94dde94ec
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-02-10 17:27:56 +0530

    fixing response in auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -24,8 +24,14 @@ authRouter.post("/signup", async (req, res) => {
       password: HashedPassword,
     });
 
-    await user.save();
-    res.send("User Added Successfully!!");
+   const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+
+    res.json({ message: "User Added Successfully!!", data: savedUser });
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
